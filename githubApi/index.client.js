8e484ab6183572bf4c6b48d9e414788d959211d2
Zachary Wu <andywu03242000@gmail.com>
2024-01-18 17:08:12 +0800

    DELETE: Deprecated token

--- githubApi/index.client.js
+++ githubApi/index.client.js
@@ -1,7 +1,5 @@
 import { Octokit } from 'octokit'
 
-const octokit = new Octokit({
-  auth: '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'
-})
+const octokit = new Octokit({})
 
 export default octokit
